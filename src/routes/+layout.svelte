9e553d56d3522aff8d4f9b31c75f64a8e1fb3c4d
ev33 <x145789@naver.com>
2025-11-06 00:11:35 +0900

    change primary font, set page data

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import favicon from "$lib/assets/favicon.svg";
-
-  let { children } = $props();
 </script>
 
 <svelte:head>
   <link rel="icon" href={favicon} />
 </svelte:head>
-<img src="/logo.svg" alt="Logo" width="164" />
-{@render children()}
+
+<header>
+  <img src="logo.svg" alt="Logo" width="164" />
+</header>
+<slot />
+<footer>This is Footer</footer>
